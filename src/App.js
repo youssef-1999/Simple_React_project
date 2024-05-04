a671fad049723d9b888fbe7f9f4b9ce741bd09d1
youssef-1999 <youssefabdelbaky4@gmail.com>
2024-05-04 20:40:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './About/About';
+import Navbar from './Navbar/Navbar';
+import Start from './Start/Start';
+import Footer from './Footer/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Start/>
+    <About/>
+    <Footer/>
+
+    </>
   );
 }
 
---
+++ src/Icon/Icon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Icon.css'
+
+function Icon() {
+  return (
+    <div>
+      <div className="icon position-relative ">
+
+<i class="fa-solid fa-star  text-white m-2"></i>
+</div>
+    </div>
+  )
+}
+
+export default Icon
